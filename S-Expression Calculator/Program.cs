99840a711907613f32aa5990542e5b3294be8c82
lemondrop81 <hacker814@hotmail.com>
2022-05-31 10:51:53 -0500

    Got the add and mulitply calculator working

--- S-Expression Calculator/Program.cs
+++ S-Expression Calculator/Program.cs
@@ -24,28 +24,83 @@ namespace S_Expression_Calculator
 
             else
             {
-                for(int i = 0; i < inputList.Count; i++)
+                var addExpression = new Program();
+                var results = addExpression.Expression(inputList);
+                
+                while(results.Count > 1)
                 {
-                    if (inputList[i].Contains(")"))
-                    {
-                        int second = int.Parse(inputList[i].Remove(inputList[i].Length - 1, 1));
-                        int first = int.Parse(inputList[i - 1]);
-                        if (inputList[i - 2].Contains("add"))
-                        {
-                            var addinstance = new Program();
-                            var addResult = addinstance.Add(second, first);
-                            inputList[i] = addResult.ToString();
-                            inputList.RemoveAt(i - 1);
-                            inputList.RemoveAt(i - 2);
-                        }
-                    }
-                } 
+                    results = addExpression.Expression(inputList);
+                }
+
+                Console.WriteLine(results[0]);
             }
         }
 
+        public List<String> Expression(List<String> inputList)
+        {
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if (inputList[i].Contains(")"))
+                {
+                    // Count how many parentheses there are
+                    int count = inputList[i].Count(f => (f == ')'));
+
+                    int second = int.Parse(inputList[i].Replace(")", ""));
+                    int first = int.Parse(inputList[i - 1]);
+                    if (inputList[i - 2].Contains("add"))
+                    {
+                        var addinstance = new Program();
+                        var addResult = addinstance.Add(second, first);
+                        string parameter = "";
+                        if (count > 1)
+                        {
+                            for (int j = 1; j < count; j++)
+                            {
+                                parameter += ")";
+                            }
+                        }
+                        inputList[i] = addResult.ToString() + parameter;
+                        inputList.RemoveAt(i - 1);
+                        inputList.RemoveAt(i - 2);
+                    }
+
+                    if (inputList[i - 2].Contains("multiply"))
+                    {
+                        var multiplyinstance = new Program();
+                        var addResult = multiplyinstance.Multiply(second, first);
+                        string parameter = "";
+                        if (count > 1)
+                        {
+                            for (int j = 1; j < count; j++)
+                            {
+                                parameter += ")";
+                            }
+                        }
+                        inputList[i] = addResult.ToString() + parameter;
+                        inputList.RemoveAt(i - 1);
+                        inputList.RemoveAt(i - 2);
+                    }
+                }
+            }
+
+            return inputList;
+        }
+
+        /// <summary>
+        /// Method to add two number together and return the sum.
+        /// </summary>
+        /// <param name="firstNumber">First number</param>
+        /// <param name="secondNumber"></param>
+        /// <returns></returns>
         public int Add(int firstNumber, int secondNumber)
         {
            int output = firstNumber + secondNumber;
+            return output;
+        }
+
+        public int Multiply(int firstNumber, int secondNumber)
+        {
+            int output = firstNumber * secondNumber;
             return output;
         }
     }
