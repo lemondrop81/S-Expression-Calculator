bffb44b5af218d6cf001c87ffa633de244f607dc
lemondrop81 <hacker814@hotmail.com>
2022-05-31 09:28:58 -0500

    Add project files.

---
+++ S-Expression Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_Expression_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var input = args;
+            int answer = 0;
+
+            if(input.Length == 1)
+            {
+                answer = int.Parse(input[0]);
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
